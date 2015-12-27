465c5daf4ef683d25a6942efd3fa14b6097d5a12
Oscar Söderlund <poscar@spotify.com>
2015-12-27 20:14:02 +0000

    Add 01-helloworld

---
+++ 01-helloworld/helloworld.asm
@@ -0,0 +1,26 @@
+; Writes "Hello, world!" to stdout
+
+global _start
+
+section .data
+
+    sys_exit: equ 1
+    sys_write: equ 4
+    fd_stdout: equ 1
+    hello_world_str: db `Hello, world!\n`
+    hello_world_str_len: equ $ - hello_world_str
+
+section .text
+
+_start:
+    nop ; gdb breakpoint
+
+    mov eax, sys_write
+    mov ebx, fd_stdout
+    mov ecx, hello_world_str
+    mov edx, hello_world_str_len
+    int 0x80
+
+    mov eax, sys_exit
+    mov ebx, 0
+    int 0x80
