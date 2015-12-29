cc76b7a932d36d09dd2218bac87bf93c62c77831
Oscar Söderlund <poscar@spotify.com>
2015-12-29 09:53:21 +0000

    Add 02-helloworld-from-c

---
+++ 02-helloworld-from-c/helloworld.asm
@@ -0,0 +1,21 @@
+global hello_world
+
+extern printf
+
+section .data
+
+    hello_world_cstr: db `Hello, world!\n`, 0
+
+section .text
+
+hello_world:
+    enter 0,0
+    pusha
+
+    push hello_world_cstr ; 32 bit address
+    call printf
+    add esp, 4 ; 32 bits = 4 bytes
+
+    popa
+    leave
+    ret
---
+++ 02-helloworld-from-c/main.c
@@ -0,0 +1,6 @@
+extern void hello_world();
+
+int main() {
+  hello_world();
+  return 0;
+}
